8b455bf1d5a276795ac1bf545e21425dd84f1755
AniFloyd <anirban7333@gmail.com>
2022-09-07 12:09:37 -0700

    UPDATE

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-document.querySelector(".nav-icon").addEventListener("click", function (){
-    document.querySelector("ul").classList.toggle("show")
-})
+E
 
 // Type Effect
   var typed3 = new Typed('#type', {
-    strings: ['Web Development', 'Digital Marketing', 'Graphics And Animations', 'Cyber Security'],
+    strings: ['Web Development', 'Digital Marketing', 'Animations', 'Cyber Security'],
     typeSpeed: 100,
     backSpeed: 50,
     smartBackspace: true, // this is a default
     loop: true
   });
+
